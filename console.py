154076822ddf86d5c7ea9edb0a6a06af4db8d89b
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-26 19:37:31 +0000

    Extensions done + horse pun added

--- console.py
+++ console.py
@@ -18,4 +18,13 @@ album_repository.save(album_1)
 album_2 = Album("2nd Album", artist_1, "Country Truck Driving Music")
 album_repository.save(album_2)
 
+artist_1.name = "A Horse Called Kevin"
+artist_repository.update(artist_1)
+album_1.title = "Saturday Night Hay Fever"
+album_repository.update(album_1)
+album_2.title = "Sticky Trotters"
+album_repository.update(album_2)
+artist_2.name = "Johny Credit"
+artist_repository.update(artist_2)
+
 pdb.set_trace()
